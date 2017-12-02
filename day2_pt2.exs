9d1880af2c8af200010266538ce119953340b494
Aaron Patterson <aaron.patterson@gmail.com>
2017-12-02 13:30:28 -0800

    golf down solution 2 a bit

--- day2_pt2.exs
+++ day2_pt2.exs
@@ -1,11 +1,12 @@
 spreadsheet = Enum.map IO.stream(:stdio, :line), fn line ->
-  with row = Enum.map String.split(line, ~r{[\t\n]}, trim: true), &String.to_integer/1
+  with row = Enum.sort Enum.map String.split(line, ~r{[\t\n]}, trim: true), &String.to_integer/1
   do
-    row2 = Enum.map row, fn left -> [left, Enum.find(row, fn right -> left != right && rem(left, right) == 0 end)] end
-    [l, r] = Enum.find row2, fn [ _, nil ] -> false
-                                [ _, _ ]   -> true
-                             end
-    round(l / r)
+    num = fn [head | rest], func ->
+      fn nil -> func.(rest, func)
+         a   -> div a, head
+      end.(Enum.find rest, &rem(&1, head) == 0)
+    end
+    num.(row, num)
   end
 end
 IO.inspect Enum.sum spreadsheet
