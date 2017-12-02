61450e931d4e39c495aa7c6a3eab545394d1c8a6
Aaron Patterson <aaron.patterson@gmail.com>
2017-12-02 12:53:29 -0800

    formatting

---
+++ day2_pt2.exs
@@ -0,0 +1,11 @@
+spreadsheet = Enum.map IO.stream(:stdio, :line), fn line ->
+  with row = Enum.map String.split(line, ~r{[\t\n]}, trim: true), &String.to_integer/1
+  do
+    row2 = Enum.map row, fn left -> [left, Enum.find(row, fn right -> left != right && rem(left, right) == 0 end)] end
+    [l, r] = Enum.find row2, fn [ _, nil ] -> false
+                                [ _, _ ]   -> true
+                             end
+    round(l / r)
+  end
+end
+IO.inspect Enum.sum spreadsheet
