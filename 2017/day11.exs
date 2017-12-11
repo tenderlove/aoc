5091632bf0270f28862a45d9a376cd87dd5d5010
Aaron Patterson <aaron.patterson@gmail.com>
2017-12-11 10:40:55 -0800

    smallerd

--- 2017/day11.exs
+++ 2017/day11.exs
@@ -1,27 +1,10 @@
 defmodule Day11 do
-  def move { x, y, z }, "n" do
-    { x, y + 1, z - 1 }
-  end
-
-  def move { x, y, z }, "ne" do
-    { x + 1, y, z - 1 }
-  end
-
-  def move { x, y, z }, "se" do
-    { x + 1, y - 1, z }
-  end
-
-  def move { x, y, z }, "s" do
-    { x, y - 1, z + 1 }
-  end
-
-  def move { x, y, z }, "sw" do
-    { x - 1, y, z + 1 }
-  end
-
-  def move { x, y, z }, "nw" do
-    { x - 1, y + 1, z }
-  end
+  def move { x, y, z }, "n"  do { x, y + 1, z - 1 } end
+  def move { x, y, z }, "ne" do { x + 1, y, z - 1 } end
+  def move { x, y, z }, "se" do { x + 1, y - 1, z } end
+  def move { x, y, z }, "s"  do { x, y - 1, z + 1 } end
+  def move { x, y, z }, "sw" do { x - 1, y, z + 1 } end
+  def move { x, y, z }, "nw" do { x - 1, y + 1, z } end
 
   def distance { a_x, a_y, a_z }, { b_x, b_y, b_z } do
     (abs(a_x - b_x) + abs(a_y - b_y) + abs(a_z - b_z)) / 2
